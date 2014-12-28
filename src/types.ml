76b7b7cfae69606b7b30410bdeda0241ec31e462
Thibault Suzanne <thi.suzanne@gmail.com>
2014-12-28 20:35:33 +0100

    Adds the Types module

---
+++ src/types.ml
@@ -0,0 +1,25 @@
+type unique = unit ref
+
+type actual
+
+type _ t =
+  | Int : actual t
+  | String : actual t
+  | Record : (Symbol.t * 'a t) list * unique -> actual t
+  | Array : 'a t * unique -> actual t
+  | Nil : actual t
+  | Unit : actual t
+  | Name : Symbol.t * 'a t option ref -> 'b t
+
+type actual_type = actual t
+
+let rec actual : type a. a t -> actual t = function ty ->
+  match ty with
+  | Name (sym, {contents = None}) -> assert false
+  | Name (_, {contents = Some t}) -> actual t
+  | Int -> ty
+  | String -> ty
+  | Record (_, _) -> ty
+  | Array (_, _) -> ty
+  | Nil -> ty
+  | Unit -> ty
